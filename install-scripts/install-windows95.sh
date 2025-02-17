892f9f09aaf469a8dcab25835e9af8727fb1eca6
Donad678 <schwabfabian99@gmail.com>
2025-02-17 15:42:22 +0100

    swapped to wget

--- install-scripts/install-windows95.sh
+++ install-scripts/install-windows95.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 
 # install windows 95 for old games
-curl -O /tmp/win95.rpm https://github.com/felixrieseberg/windows95/releases/download/v3.1.1/windows95-3.1.1-1.x86_64.rpm
+wget -O /tmp/win95.rpm https://github.com/felixrieseberg/windows95/releases/download/v3.1.1/windows95-3.1.1-1.x86_64.rpm
 dnf5 install -y /tmp/win95.rpm
