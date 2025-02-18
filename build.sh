dde2d1c81234eb7eeea56699f0e94b8b9d09f583
Donad_VR <schwabfabian99@gmail.com>
2025-02-18 13:35:30 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,6 +18,7 @@ dnf5 install -y lact
 # install dotnet 8/9
 dnf5 install -y dotnet-sdk-8.0 dotnet-sdk-9.0
 
+dnf5 install -y kernel-devel kernel-headers gcc make patch
 # install vmware
 #/tmp/install-scripts/install-vmware.sh
 wget -O /tmp/vmware.bundle https://stream.donadvr.de/public/vmware-workstation-pro-17.bundle
