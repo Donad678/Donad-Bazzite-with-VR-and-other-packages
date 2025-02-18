0afc80a3c6eef2cc91cc8a43cb208d02bc35820f
Donad_VR <schwabfabian99@gmail.com>
2025-02-18 12:54:26 +0100

    Update install-vmware.sh

--- install-scripts/install-vmware.sh
+++ install-scripts/install-vmware.sh
@@ -2,4 +2,4 @@
 
 wget -O /tmp/vmware.bundle https://stream.donadvr.de/public/vmware-workstation-pro-17.bundle
 
-EULAS_AGREED=1 /tmp/binaries/vmware.bundle --deferred-gtk --console 
+/tmp/binaries/vmware.bundle
