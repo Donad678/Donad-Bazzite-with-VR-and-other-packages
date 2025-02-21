08c186af45fd7db6e05c7e93948f015a42fa35fc
Donad678 <schwabfabian99@gmail.com>
2025-02-21 14:45:33 +0100

    update virtual here

--- install-scripts/install-virtualhere-server.sh
+++ install-scripts/install-virtualhere-server.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
 
-curl https://raw.githubusercontent.com/virtualhere/script/main/install_server | sh
+# curl https://raw.githubusercontent.com/virtualhere/script/main/install_server | sh
+
+wget https://www.virtualhere.com/sites/default/files/usbserver/vhusbdx86_64
+chmod +x vhusbdx86_64
+mv vhusbdx86_64 /usr/bin
+mkdir -p /usr/local/etc/virtualhere
+if [ -d "/etc/systemd/system" ]; then
+  cat << EOF > /etc/systemd/system/virtualhere.service
+[Unit]
+Description=VirtualHere Server
+After=network.target
+
+[Service]
+Type=forking
+ExecStart=/usr/bin/vhusbdx86_64 -b -c /usr/local/etc/virtualhere/config.ini
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+systemctl enable virtualhere.service
