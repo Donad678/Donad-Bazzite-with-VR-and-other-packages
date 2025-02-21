9d70e0b998fd6a0c7664912328f0d2e3d2b665ae
Donad678 <schwabfabian99@gmail.com>
2025-02-21 14:52:25 +0100

    add service

--- install-scripts/install-virtualhere-server.sh
+++ install-scripts/install-virtualhere-server.sh
@@ -6,17 +6,6 @@ wget https://www.virtualhere.com/sites/default/files/usbserver/vhusbdx86_64
 chmod +x vhusbdx86_64
 mv vhusbdx86_64 /usr/bin
 if [ -d "/etc/systemd/system" ]; then
-  cat << EOF > /etc/systemd/system/virtualhere.service
-[Unit]
-Description=VirtualHere Server
-After=network.target
-
-[Service]
-Type=forking
-ExecStart=/usr/bin/vhusbdx86_64 -b -c /usr/local/etc/virtualhere/config.ini
-
-[Install]
-WantedBy=multi-user.target
-EOF
+  cp /tmp/misc/virtualhere.service /etc/systemd/system/virtualhere.service
 
 systemctl enable virtualhere.service
