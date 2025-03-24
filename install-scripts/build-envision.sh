2672fa2be803e4a9cde661ab14b10917862694e4
Donad678 <schwabfabian99@gmail.com>
2025-03-24 17:13:15 +0100

    envision deps

--- install-scripts/build-envision.sh
+++ install-scripts/build-envision.sh
@@ -1,8 +1,7 @@
 # build envision
 # desktop dependencys
 dnf5 install -y meson rustc gtk4-devel gtk4-devel cargo openssl-devel libadwaita-devel vte-2.91-gtk4
-#wivrn deps
-dnf5 install avahi-devel avahi-glib-devel cli11-devel gstreamer1-plugins-base-devel gstreamer1-devel ffmpeg-devel ffmpeg-devel ffmpeg-devel libnotify-devel pipewire-devel ffmpeg-devel libva-devel json-devel x264-devel
+dnf5 install -y envision-monado envision-wivrn
 git clone https://gitlab.com/gabmus/envision/
 cd envision
 meson setup build -Dprefix="/usr" -Dprofile=development
