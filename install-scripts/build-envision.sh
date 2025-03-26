8142fa40823e42642cf89e484a3ee22a51bb3eb7
Donad678 <schwabfabian99@gmail.com>
2025-03-26 15:56:37 +0100

    envision changes

--- install-scripts/build-envision.sh
+++ install-scripts/build-envision.sh
@@ -1,6 +1,7 @@
 # build envision
 # desktop dependencys
 dnf5 install -y meson rustc gtk4-devel gtk4-devel cargo openssl-devel libadwaita-devel vte-2.91-gtk4
+dnf5 install -y envision-monado envision-wivrn
 git clone https://gitlab.com/gabmus/envision/
 cd envision
 meson setup build -Dprefix="/usr" -Dprofile=development
