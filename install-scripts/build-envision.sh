ea0eb525cef2609b4e9a813fd404fc6ad3b3f3c7
Donad_VR <schwabfabian99@gmail.com>
2025-04-10 10:48:09 +0200

    Update build-envision.sh test

--- install-scripts/build-envision.sh
+++ install-scripts/build-envision.sh
@@ -4,7 +4,7 @@ set -ouex pipefail
 # desktop dependencys
 dnf5 install -y cmake eigen3-devel gcc-c++ git-lfs glslang-devel glslc libbsd-devel mesa-libGL-devel systemd-devel libX11-devel libxcb-devel libXrandr-devel ninja-build opencv-devel openxr-devel SDL2-devel vulkan-devel vulkan-loader-devel wayland-devel wayland-protocols-devel
 dnf5 install -y meson rustc gtk4-devel gtk4-devel cargo openssl-devel libadwaita-devel vte-2.91-gtk4 clang-devel
-#dnf5 install -y envision-monado envision-wivrn
+dnf5 install -y envision-monado envision-wivrn
 git clone https://gitlab.com/gabmus/envision/
 cd envision
 meson setup build -Dprefix="/usr" -Dprofile=development
