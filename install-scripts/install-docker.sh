2bed340c356ed617b8975b37c88c1050efdb3bf0
Donad678 <schwabfabian99@gmail.com>
2025-10-05 01:31:39 +0200

    enable containerd service

--- install-scripts/install-docker.sh
+++ install-scripts/install-docker.sh
@@ -3,3 +3,4 @@ dnf5 -y install dnf-plugins-core
 dnf5 config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 dnf5 -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 systemctl enable docker
+systemctl enable containerd.service
