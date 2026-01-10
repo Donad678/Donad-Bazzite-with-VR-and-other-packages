87f78127d1a713da4c3f94ff0bbb83334e319b36
Donad678 <schwabfabian99@gmail.com>
2026-01-10 10:42:47 +0100

    added disclaimer

--- install-scripts/install-custom-scripts.sh
+++ install-scripts/install-custom-scripts.sh
@@ -2,3 +2,8 @@
 # Install Scripts from misc/Tools to /usr/bin
 
 cp -rv /tmp/misc/Tools/* /usr/bin
+
+# The lighthouse tool is from
+# https://github.com/ShayBox/Lighthouse
+
+# The others I wrote myself
