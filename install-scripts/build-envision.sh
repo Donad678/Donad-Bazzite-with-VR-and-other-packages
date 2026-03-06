fc19df3ca009a085c4a7eada6722097a07d2b323
Donad678 <schwabfabian99@gmail.com>
2026-03-06 02:06:22 +0100

    Add force restart in addition to udev rule
    udev rule is unreliable for now, adding force restart to envision

--- install-scripts/build-envision.sh
+++ install-scripts/build-envision.sh
@@ -25,7 +25,7 @@ dnf5 -y install xr-hardware
 /usr/bin/setcap CAP_SYS_NICE=eip /usr/bin/monado-service
 
 # Edit Envision .desktop to force restart headset on start
-#desktop-file-edit --set-key="Exec" --set-value="/usr/bin/forceRestartIndex envision" "/usr/share/applications/org.gabmus.envision.Devel.desktop"
+desktop-file-edit --set-key="Exec" --set-value="/usr/bin/forceRestartIndex envision" "/usr/share/applications/org.gabmus.envision.Devel.desktop"
 
 # fix index on boot
 install -m 644 -o root -g root "/tmp/misc/udev/60-valve-index-reboot.rules" "/etc/udev/rules.d/60-valve-index-reboot.rules"
