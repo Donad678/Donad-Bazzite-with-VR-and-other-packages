486539f51c8f26da9cb03f8d410f96ea6dfc9b3e
Donad678 <schwabfabian99@gmail.com>
2026-03-15 15:59:48 +0100

    add handful of dependencies

--- install-scripts/install-envision.sh
+++ install-scripts/install-envision.sh
@@ -9,7 +9,7 @@ dnf5 -y install envision-nightly --allowerasing
 dnf5 install -y glslc libusb1 onnx-libs onnxruntime opencv SDL3 libbsd x264-libs bluez-libs vulkan-loader openxr
 dnf5 install -y 'opencv*' --exclude='*devel*' --exclude='*doc*' --exclude='*examples*' --exclude='*java*' --allowerasing
 
-dnf5 install -y openvr-api libwayland-client
+dnf5 install -y openvr-api openxr-libs libwayland-client
 # Install Monado binary for envision to make new installations easier
 #dnf5 -y copr enable joviatrix/monado-git
 #dnf5 -y install monado
