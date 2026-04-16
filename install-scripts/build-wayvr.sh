9059e97cc8fa03021968c2e01c6455d8ef16b897
Donad_VR <schwabfabian99@gmail.com>
2026-04-16 14:25:12 +0200

    adjust --setopt=disable_excludes=*

--- install-scripts/build-wayvr.sh
+++ install-scripts/build-wayvr.sh
@@ -1,7 +1,7 @@
 # build wayvr based on the pkgbuild from https://aur.archlinux.org/packages/wayvr-git
 set -ouex pipefail
 
-dnf5 install -y --disableexcludes=all pipewire-devel --best --allowerasing
+dnf5 --setopt=disable_excludes=* install -y pipewire-devel --best --allowerasing
 dnf5 install -y cmake libxkbcommon libxkbcommon-devel fontconfig fontconfig-devel dbus dbus-devel alsa-lib alsa-lib-devel libshaderc-devel openssl-devel python3 wayland-devel pkgconf-pkg-config
 
 STAGING_DIR="/tmp/staging"
